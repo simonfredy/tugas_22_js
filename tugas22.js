6f9f4a32622b6186f5be46c1e3329773dfd9a2e2
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-28 12:55:34 +0700

    Tugas 22 JS

---
+++ tugas22.js
@@ -0,0 +1,10 @@
+function tugasDuaPuluhDua() {
+    var dataStr = "Saya ingin belajar bersama";
+    var dataArr = dataStr.split(" ");
+
+    dataArr.forEach(function(item, index){
+        console.log('Item : ', item, 'Index ke ', index);
+    });
+}
+
+tugasDuaPuluhDua();
